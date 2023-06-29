b8c3d5143cc22be343b44ffa0f54dd8b1d18f15b
João Pedro Silva Cunha <jpcunha5@gmail.com>
2023-06-28 22:10:02 -0300

    Encerrando o README

--- main.py
+++
@@ -1,3 +0,0 @@
-import functions as f
-
-f.main ()
---
+++ src/main.py
@@ -0,0 +1,7 @@
+import functions as f
+
+# Inteligência Artificial 2023.1
+# Alunos: João Gabriel Alvez de Souza, João Pedro Silva Cunha
+# Trabalho Prático - Algoritmo KNN
+
+f.main ()
